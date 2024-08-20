706eda1ff112914f9d34616792df7f713c6989b4
Lucía Bonjour <lucia.bonjouramicone@gmail.com>
2024-08-19 21:49:35 -0300

    Update login.js
    faltaba codigo de branch de lu

--- js/login.js
+++ js/login.js
@@ -20,19 +20,17 @@ icon.addEventListener("click", function() {
     contraseña.type = "password";
   }
 })
+     if (inputUsuario === '' || inputContraseña === '') {
+      alert('Completar usuario y contraseña');
+    } else {
 
-    // if (inputUsuario === 'user' && inputContraseña === 'password') {
-        // Guardar sesión en localStorage
+  // Guardar sesión en localStorage
         localStorage.setItem('loggedIn', 'true');
         localStorage.setItem('username', inputUsuario);
         window.location.href = 'https://laraolivares.github.io/workspace-inicial/';
-    
-  // NO SE COMO SE HACE PARA VERIFICAR SI EL USUARIO Y LA CONTRASEÑA ESTAN OK. DONDE SE ALMACENA ESA INFO??
-
-  //} else {
-  //  alert('Usuario o contraseña incorrectos');
-    }
-//  }
+  
  });
 });
+// NO SE COMO SE HACE PARA VERIFICAR SI EL USUARIO Y LA CONTRASEÑA ESTAN OK. DONDE SE ALMACENA ESA INFO??
+    
 
