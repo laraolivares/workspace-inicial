4526641949d607ffcc6323646a6080502a665e46
LaraOlivares <laraolivaresdimatteo@gmail.com>
2024-10-10 22:06:13 -0300

    dropdown menu

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,7 +1,36 @@
+function setCatID(id) {
+  localStorage.setItem("catID", id);
+  window.location = "products.html";
+}
+
+
+function logout() {
+  localStorage.removeItem('loggedIn');
+  localStorage.removeItem('username');
+  window.location.href = 'login.html';
+}
+
+window.onload = function() {
+  const estaLogueado = localStorage.getItem('loggedIn');
+  if (!estaLogueado) {
+      window.location.href = 'login.html';
+  } else {
+      const username = localStorage.getItem('username');
+      document.getElementById("username").innerHTML = username;
+  }
+};
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const profileImage = document.getElementById("profileImage");
     const imageInput = document.getElementById("imageInput");
     const saveImageBtn = document.getElementById("saveImage");
+    const cerrarSesion = document.getElementById("cerrarSesion"); 
+
+                    // Cerrar sesion desde el dropdown menu
+                    if (cerrarSesion) {
+                        cerrarSesion.addEventListener('click', logout);
+                    }
   
     // Cargar la imagen del localStorage si está guardada
     const storedImage = localStorage.getItem("profileImage");
